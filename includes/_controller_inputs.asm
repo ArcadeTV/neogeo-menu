7f1ff0c37f97a24dfe58f01110fb1788b7528820
ArcadeTV <info@arcade-tv.de>
2023-10-24 12:30:50 +0200

    fixed another bug in left-right navigation

--- includes/_controller_inputs.asm
+++ includes/_controller_inputs.asm
@@ -77,52 +77,42 @@ readControllerInputs:
     btst.b  #CNT_LEFT,p1_Repeat
     beq.w   .noleft
     nop                                     ; LEFT pressed
-
-    move.b  RAM_CurrentIndex,d0
-    move.b  RAM_CurrentListPos,d3
-
-    cmp.b   #EntriesPerPage,d0              ; if (CurrentIndex < #EntriesPerPage)
-    bcs.s   .first_to_last_page
-
-    sub.b   #EntriesPerPage,d0              ; CurrentIndex - #EntriesPerPage
-    bra     .not_last_page
-
-.first_to_last_page:
-;    move.b  #GamesCount,d1
-;    move.b  #TotalListCount,d2
-;    sub.b   d3,d1                           ; #GamesCount - RAM_CurrentListPosR
-;    sub.b   d1,d2                           ; #TotalListCount - (#GamesCount - RAM_CurrentListPos)
-;    move.b  RAM_CurrentIndex,d0             
-;    sub.b   d2,d0                           
-;    cmp.b   #GamesCount-1,d0
-;    bhi.s   .adjustLastPosition             ; if index is greater than last games' index
-
-    move.b  RAM_CurrentIndex,d0
-    move.b  #EntriesPerPage,d1
-    move.b  #GamesCount,d2
-
-    sub.b   d1, d0                          ; d0 = RAM_CurrentIndex - EntriesPerPage
-
-                                            ; Addieren von GamesCount, um sicherzustellen, dass das Ergebnis positiv ist
-                                            ; Dies ist ein Trick, um mit negativen Zahlen in der Modulo-Operation umzugehen
-    add.b   d2, d0                          ; d0 = RAM_CurrentIndex - EntriesPerPage + GamesCount
-
-.left_modulo_loop:
-                                            ; Wenn RAM_CurrentIndex - EntriesPerPage + GamesCountn < GamesCount, sind wir fertig
-    cmp.b   d2, d0                          ; vergleiche d0 mit GamesCount
-    blt.s   .end_left_modulo                ; wenn d0 < d2, zum label "end_left_modulo" springen
-                                            ; Ansonsten, subtrahiere GamesCount von D0 und wiederhole
-    sub.b   d2, d0
-    bra.s   .left_modulo_loop
-.end_left_modulo:
-
 ;
-;
-    bra     .not_last_page
-.adjustLastPosition
-    move.b  #GamesCount-1,d0                ; set index to last existing game
-.not_last_page:
+;------------------
+    move.b  #TotalPages,d0                  ; 
+    cmp.b   #1,d0                           ;
+    beq     .noleft                         ; branch out if we only have 1 page
 
+    move.b  RAM_CurrentIndex,d0             ; 
+    tst.b   RAM_CurrentPage                 ; if we are on any other page than the first,
+    bne     .LEFT_else                      ; just skip to the prev page
+
+    cmp.b   #EntriesPerPage,d0              ; 
+    bge     .LEFT_else                      ; if RAM_CurrentIndex >= EntriesPerPage, just skip to the prev page
+
+                                            ; if (RAM_CurrentIndex < EntriesPerPage):
+    move.b  #TotalListCount,d1              ; D1 = TotalListCount
+    move.b  #EntriesPerPage,d2              ; D2 = EntriesPerPage
+    sub.b   RAM_CurrentListPos,d2           ; D2 = EntriesPerPage - RAM_CurrentListPos
+    sub.b   d2,d1                           ; D1 = TotalListCount - (EntriesPerPage - RAM_CurrentListPos)
+    move.b  d1,d0                           ; D0 = NextIndex
+
+    move.b  #GamesCount,d1                  ; D1 = GamesCount
+    sub.b   #1,d1                           ; D1 = GamesCount - 1
+    cmp.b   d1,d0                           ; Compare NextIndex to (GamesCount - 1)
+    ble     .LEFT_done                      ; if NextIndex <= (GamesCount - 1), branch out
+
+    move.b  d1,d0                           ; D0 = NextIndex = GamesCount - 1
+    bra     .LEFT_done                      ; branch out
+
+.LEFT_else:
+    SUB.B   #EntriesPerPage,d0              ; D0 = RAM_CurrentIndex - EntriesPerPage
+    bra     .LEFT_done                      ; 
+
+.LEFT_done:
+
+;------------------
+;
     move.b  d0,RAM_CurrentIndex
     jsr     updateList
 
@@ -130,44 +120,51 @@ readControllerInputs:
     btst.b  #CNT_RIGHT,p1_Repeat
     beq     .noright
     nop                                     ; RIGHT pressed
-                                            
-    move.b  RAM_CurrentPage,d0
-    cmp.b   #TotalPages-2,d0                ; Compare currentPage to TotalPages-2
-    beq     .page_before_last_page          ; branch if equal
+;-------------------------
+    move.b  #TotalPages,d0 
+    cmp.b   #1,d0 
+    beq     .noright                        ; branch out if we only have 1 page
 
-                                            ; Case: currentPage != TotalPages-2
-                                            
-    cmp.b   #TotalPages-1,d0                ; Compare currentPage to TotalPages-1
-    beq     .last_page                      ; branch if equal
-
-                                            ; Case: currentPage != TotalPages-1
-                                            ; Calculate Index = Index+EntriesPerPage
-    move.b  RAM_CurrentIndex,d0             ; get currentIndex in d0
-    add.b   #EntriesPerPage,d0              ; Add EntriesPerPage to d0
-    bra     .end_of_calc                    ; Branch out
-
-.page_before_last_page:                     ; Case: currentPage == TotalPages-2
-                                            ; Calculate Index + EntriesPerPage
     move.b  RAM_CurrentIndex,d0
-    move.b  #EntriesPerPage,d1
-    move.b  #GamesCount,d2
+    move.b  RAM_CurrentPage,d1
+    move.b  RAM_CurrentListPos,d2
 
-    add.b   d1, d0                          ; d0 = RAM_CurrentIndex - EntriesPerPage
-                                            ; Add GamesCount to ensure a positive result
-                                            ; This trick helps avoiding negative numbers in the  modulo-operation
-.right_modulo_loop:
-                                            ; If RAM_CurrentIndex - EntriesPerPage + GamesCountn < GamesCount -> we are done
-    cmp.b   d2, d0                          ; compare d0 to GamesCount
-    blt.s   .end_right_modulo               ; If d0 < d2, branch to "end_right_modulo"
-    sub.b   d2, d0                          ; else substract GamesCount from D0 and repeat
-    bra.s   .right_modulo_loop
-.end_right_modulo:
-    bra     .end_of_calc                    ; Branch out
+    cmp.b   #TotalPages-1,d1
+    beq     .lastPage
+    cmp.b   #TotalPages-2,d1
+    beq     .pageBeforeLastPage
 
-.last_page:                                 ; Case: currentPage == TotalPages-1
-    move.b  RAM_CurrentListPos,d0           ; Index = CurrentListPos
+    bra     .Right_defaultCase
 
-.end_of_calc:
+    ; #EntriesPerPage
+    ; #TotalPages
+    ; #GamesCount
+    ; #ListOverhead
+
+    bra     .Right_end
+
+.pageBeforeLastPage:
+    ; if currentIndex+EntriesPerPage <= GamesCount-1 => default
+    ; else d0=GamesCount-1
+    add.b   #EntriesPerPage,d0
+    cmp.b   #GamesCount-1,d0
+    bls     .Right_defaultCase
+    move.b  #GamesCount-1,d0
+    bra     .Right_end
+
+.lastPage:
+    move.b  d2,d0
+    bra     .Right_end
+
+    ; if GamesCount > CurrentPos => d0=CurrentPos, else d0=GamesCount-1
+
+.Right_defaultCase:
+    move.b  RAM_CurrentIndex,d0
+    add.b   #EntriesPerPage,d0
+
+.Right_end:
+
+;-------------------------                                            
     move.b  d0,RAM_CurrentIndex
     jsr     updateList
 
