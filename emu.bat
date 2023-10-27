cc3dbe84ff219173d16726cfa5a374563b050646
ArcadeTV <info@arcade-tv.de>
2023-10-27 13:47:21 +0200

    edited S rom with vortex logo

--- emu.bat
+++ emu.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Please adjust the path to your MAME folder according to your system
 REM You can remove the -debug option or press F5 when MAME starts.
-start "" /d D:\MAME "D:\MAME\mame.exe" neogeo -cart1 menu -debug
+start "" /d C:\VASM_BUILDING\NG_MENU\MAME ".\MAME\mame.exe" neogeo -cart1 menu -debug
