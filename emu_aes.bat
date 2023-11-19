042f5753996c7caa6cd840503fc303c8bac2e8cb
ArcadeTV <info@arcade-tv.de>
2023-11-19 14:28:09 +0100

    added emu_aes.bat

--- emu_aes.bat
+++ emu_aes.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Please adjust the path to your MAME folder according to your system
-start "" /d C:\VASM_BUILDING\NG_MENU\MAME ".\MAME\mame.exe" neogeo -cart1 menu
+start "" /d C:\VASM_BUILDING\NG_MENU\MAME ".\MAME\mame.exe" aes menu -bios japan
